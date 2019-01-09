cc4e7b196fb57e90d4fd2e8ea9fdc313a9648ec9
William Lan <wlan76@yahoo.com>
2019-01-09 09:32:52 -0800

    Copy of the HWs

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsArray = [
+	{
+	  "name":"Goofy",
+	  "photo":"https://www.celebrationspress.com/wp-content/uploads/2018/01/010218Goofy.jpg",
+	  "scores":[
+	     5,
+	     1,
+	     4,
+	     4,
+	     5,
+	     1,
+	     2,
+	     5,
+	     4,
+	     1
+      		]
+	}
+]
+
+
+module.exports = friendsArray;
