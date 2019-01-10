03c40a9b2ed481bfee41bc30920b02e7b5ed14c3
William Lan <wlan76@yahoo.com>
2019-01-10 09:51:56 -0800

    update2

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 
-require('./app/routing/api-routes.js')(app);
-require('./app/routing/html-routes.js')(app);
+require('./app/routing/apiroutes.js')(app);
+require('./app/routing/htmlroutes.js')(app);
